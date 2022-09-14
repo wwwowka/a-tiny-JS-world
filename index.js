f9010eb8d7f9751f9d441a60f77ad8d16e472735
Volodymyr Vyshnyakov <wwwowka@gmail.com>
2022-09-14 17:47:33 +0300

    it works

--- index.js
+++ index.js
@@ -8,9 +8,61 @@ import { print } from './js/lib.js';
 
 // ======== OBJECTS DEFINITIONS ========
 // Define your objects here
+   const dog = {
+      species: 'dog',
+      name: 'Life',
+      gender: 'male',
+      legs: 4,
+      hands: 0,
+      saying: 'woof-woof!'
+    };
 
+    const cat = {
+      species: 'cat',
+      name: 'Mia',
+      gender: 'female',
+      legs: 4,
+      hands: 0,
+      saying: 'meow-yuu'
+    };
 
+    const man = {
+       species: 'human',
+       name: 'Bob',
+       gender: 'male',
+       legs: 2,
+       hands: 2,
+       saying: 'Are you ready kids?'
+     };
+
+     const woman = {
+        species: 'human',
+        name: 'Ann',
+        gender: 'female',
+        legs: 4,
+        hands: 0,
+        saying: 'Hello!'
+      };
+      
+     const catWoman = Object.create(cat);
+     catWoman.species = 'cat-human';
+     catWoman.name = 'Selina';
+     catWoman.legs = 2;
+     catWoman.hands = 2;
+
+     dog.friends = cat.name + ', ' + man.name + ', ' + woman.name + ', ' + catWoman.name;
+     cat.friends = '-';
+     man.friends = dog.name + ', ' + woman.name;
+     woman.friends = cat.name + ', ' + man.name + ', ' + catWoman.name;
+    
 // ======== OUTPUT ========
+const inhabitants = [dog, cat, woman, man, catWoman]
+const keys = ["species", "name", "gender", "legs", "hands", "saying", "friends"]
+
+inhabitants.map((inhabitant) =>
+    print(keys.map((key) => inhabitant[key]).join("; "))
+)
+
 /* Use print(message) for output.
    Default tag for message is <pre>. Use print(message,'div') to change containing element tag.
 
@@ -19,8 +71,8 @@ import { print } from './js/lib.js';
    so code reviewers might focus on a single file that is index.js.
    */
 
-/* Print examples:
-   print('ABC');
+ //Print examples:
+ /*  print('ABC');
    print('<strong>ABC</strong>');
    print('<strong>ABC</strong>', 'div');
 
